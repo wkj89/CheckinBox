2231c250cab02526b51e6b5bab16bef8535f9cca
Michael_Wang <wkj89@qq.com>
2020-10-30 15:26:49 +0800

    Update C189Checkin.py

--- Cloud189Checkin/C189Checkin.py
+++ Cloud189Checkin/C189Checkin.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
     import os,json
     data=os.getenv('userinfo')
     users=json.loads(data)
-    for k,v in a.items():
+    for k,v in users.items():
         username=k
         password=v
         main()
